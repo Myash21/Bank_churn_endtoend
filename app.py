18fc6f9aaf61537ba443a6040bffad8a83fa0451
Yash Mhaskar <myash100@gmail.com>
2024-02-17 08:41:46 -0800

    fifth commit

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-# app.py
-
-import pickle
+import joblib
 from flask import Flask, jsonify, render_template, request
 import numpy as np
 
 app = Flask(__name__)
 
 # Load the model
-classmodel = pickle.load(open('bank_churn.pkl', 'rb'))
+classmodel = None  # Load model dynamically
 
 # Home page
 @app.route('/')
@@ -17,6 +15,9 @@ def home():
 # API endpoint for prediction
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
+    global classmodel
+    if classmodel is None:
+        classmodel = joblib.load('bank_churn.joblib')
     data = request.json['data']
     input_data = np.array(list(data.values())).reshape(1, -1)
     output = classmodel.predict(input_data)
@@ -26,6 +27,9 @@ def predict_api():
 # Prediction route
 @app.route('/predict', methods=['POST'])
 def predict():
+    global classmodel
+    if classmodel is None:
+        classmodel = joblib.load('bank_churn.joblib')
     data = [float(x) for x in request.form.values()]
     final_input = np.array(data).reshape(1, -1)
     output = classmodel.predict(final_input)[0]
@@ -34,5 +38,4 @@ def predict():
     prediction_text = "Bank churn likely" if output == 1 else "Bank churn unlikely"
     
     return render_template("home.html", prediction_text=prediction_text)
-
 
